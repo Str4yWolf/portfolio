4a45fd4f3e1f2110f9aa00bfb11260ec41b957c8
Michael Huang <michaelhuang@hotmail.de>
2020-09-16 14:57:15 +0200

    Small changes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,10 +3,10 @@ import MaterialIcon from '../components/MaterialIcon'
 
 const About = () => {
     return <div id="about" className="about">
-        <h1>Hey everyone, I'm Michael.</h1>
+        <h1>Hey there, I'm Michael.</h1>
         <p>
         
-            I've been a <strong>passionate web developer for 2 years</strong> and have a Computer Science background. My biggest fields of interest include productivity tools, e-learning, and information and communication platforms.
+            A <strong>passionate web developer with 2 years experience</strong> and a Computer Science background. My biggest fields of interest include productivity tools, e-learning, and information and communication platforms.
             <br />
             <br />
                 I enjoy <strong>collaborating</strong> with different and diverse teams as they provide a great environment to exchange new ideas and grow one's field of vision.
@@ -15,7 +15,7 @@ const About = () => {
                 My vision is to <strong>contribute</strong> to human society as a driver of growth and also to encourage interdisciplinary and intercultural interaction, as I believe we can learn a lot from one another.
             <br />
             <br />
-                In general, I'd describe myself as a passionate <strong>problem solver</strong> with a knack for cultures, languages, and travel. I also like to contemplate life and try to get the most out of it - e.g., through life hacks.
+                In general, I'd describe myself as a dedicated <strong>problem solver</strong> with a knack for cultures, languages, and travel. I also like to contemplate life and try to get the most out of it - e.g., through life hacks.
             <br />
             <br />
             <br />
